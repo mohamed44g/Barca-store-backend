5e0ecdf78d4a9da405200f2fc490c634ef96ac15
Mohamed badry <85965151+mohamed44g@users.noreply.github.com>
2024-08-08 12:12:42 +0200

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -9,7 +9,7 @@ export const app = express();
 const port = process.env.PORT || 3000;
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://192.168.1.4:5173",
     credentials: true,
   })
 );
